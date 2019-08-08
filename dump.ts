b3820053631c1fa024960ade277cd762dd1e828d
lfc2007 <45638176+lfc2007@users.noreply.github.com>
2019-08-08 21:01:45 +0100

    Delete dump.ts

--- dump.ts
+++
@@ -1,6 +0,0 @@
-this.router.events.subscribe((event) => {
-      if(event instanceof NavigationEnd) {
-        this.slId = this.router.routerState.snapshot.root.queryParamMap.get('slid');
-        this.planNumber = this.router.routerState.snapshot.root.queryParamMap.get('planNumber');
-      }
-    });
